08e91697c114b56de8da349df4d33aabdc45658e
pawelbensellim <pawel.bensel@lifeinmobile.com>
2020-01-03 11:25:09 +0100

    setup DB connection  + simple test command

--- config/database.php
+++ config/database.php
@@ -62,7 +62,23 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-
+        'ris' => [
+            'driver' => env('RIS_CONNECTION'),            
+            'host' => env('RIS_HOST', '127.0.0.1'),
+            'port' => env('RIS_PORT', '3306'),
+            'database' => env('RIS_DATABASE', 'forge'),
+            'username' => env('RIS_USERNAME', 'forge'),
+            'password' => env('RIS_PASSWORD', ''),            
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
