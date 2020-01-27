aadfbd651f1ad8940181ed2d36a1b4f9f76ff3f9
Paweł Lipka <bmb@pop-os.localdomain>
2020-01-27 23:06:41 +0100

    uncomment try catch block

--- app/Console/Commands/ParseCommand.php
+++ app/Console/Commands/ParseCommand.php
@@ -57,7 +57,7 @@ class ParseCommand extends Command
         }else {
             while($data = $source->getNextData()) {
                 foreach ($data as $row){
-                    //try {
+                    try {
                         $this->officeService->setSourceRowId($row['source_row']['source_row_id']);
                         $officeId = $this->officeService->getId($row['office']);
                         $currentOffice = Office::find($officeId);
@@ -67,13 +67,12 @@ class ParseCommand extends Command
                         $this->agentService->getId($row['agent']);
 
 
-                    //}catch (\Exception $e){
-                    //    echo 'Exception';
-                     //   Log::channel($this->argument('source'))->error('Could not parse data', (array) $e);
-                    //}
+                    }catch (\Exception $e){
+                       echo 'Exception';
+                       Log::channel($this->argument('source'))->error('Could not parse data', (array) $e);
+                    }
                 }
             }
         }
-
     }
 }
